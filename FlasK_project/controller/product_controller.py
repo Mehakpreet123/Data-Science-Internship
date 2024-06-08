ec87e8bb2506a3f46fe5ca590b993ef3f4297560
Mehakpreet Kaur <114038404+Mehakpreet123@users.noreply.github.com>
2024-06-08 15:10:22 +0530

    Delete FlasK_project directory

--- FlasK_project/controller/product_controller.py
+++
@@ -1,4 +0,0 @@
-from app import app   # first app is file name app.py second is variable app in that file
-@app.route("/product/add") # to handle req at endpoint
-def p_add():
-    return "This is product add operation"
